599d96fca07bda6b8c9b35de43305ded30e3e3f7
Eaulmesse <theo.guerin01120@gmail.com>
2022-02-21 13:48:43 +0100

    feat: Finishing the JS

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-// Elements du DOM
-const divVies = document.querySelector(".vies");
-const message = document.getElementById("message");
-const formulaire = document.getElementById("inputBox");
-const input = document.getElementById("input");
-const essayerBtn = document.getElementById("essayerBtn");
-const rejouerBtn = document.getElementById("rejouer");
-const body = document.getElementsByTagName("body")[0];
+const divVies = document.querySelector('.vies');
+const message = document.getElementById('message');
+const formulaire = document.getElementById('inputBox');
+const input = document.getElementById('number');
+const essayerBtn = document.getElementById('essayerBtn');
+const rejouerBtn = document.getElementById('rejouer');
+const body = document.getElementsByTagName('body')[0];
 
 // Modèle de coeurs
 const coeurVide = '<ion-icon name="heart-outline"></ion-icon>';
 const coeurPlein = '<ion-icon name="heart"></ion-icon>';
 
-// Fonds
-
+// Fond 
 const bgFroid = 'linear-gradient(120deg, #89f7fe 0%, #66a6ff 100%)';
 const bgTiède = 'linear-gradient(to top, #d299c2 0%, #fef9d7 100%)';
 const bgChaud = 'linear-gradient(to right, #f78ca0 0%, #f9748f 19%, #fd868c 60%, #fe9a8b 100%)';
@@ -20,57 +18,83 @@ const bgBrulant = 'linear-gradient(to top, #ff0844 0%, #ffb199 100%)';
 
 const bgWin = 'linear-gradient(to right, #f83600 0%, #f9d423 100%)';
 const bgLoose = 'linear-gradient(to top, #09203f 0%, #537895 100%)';
-
-// PLAY
-
+// PLAY : 
 const play = () => {
-    
-    // nombre aleatoire
-    const randomNumber = Math.floor(Math.random() * 101);
+
+    // nombre aléatoire 
+    const randomNumber = Math.floor(Math.random() * 101); // 3
     const totalVies = 6;
     let vies = totalVies;
     console.log(randomNumber);
 
-    //Actualisation
-    formulaire.addEventListener('submit',(e) => {
+    // Actualisation à chaque essai - TOUTE LA LOGIQUE
+    formulaire.addEventListener('submit', (e) => {
         e.preventDefault();
-        const valeurInput = parseint(input.value); 
+        const valeurInput = parseInt(input.value);
 
-        if(valeurInput < 0 || valeurInput > 100) return;
+        if (valeurInput < 0 || valeurInput > 100) return;
 
-        if(valeurInput === randomNumber){
+        if (valeurInput === randomNumber) {
             body.style.backgroundImage = bgWin;
-            message.texteContent = `Bravo !!! Le nombre était bien ${randomNumber}`;
+            message.textContent = `BRAVO !!! Le nombre était bien ${randomNumber}`;
             rejouerBtn.style.display = "block";
+            essayerBtn.setAttribute("disabled", "");
         }
 
-        if(valeurInput !==randomNumber) {
-            if(randomNumber < valeurInput + 3 && randomNumber > valeurInput - 3){
+        if (valeurInput !== randomNumber) {
+            if (randomNumber < valeurInput + 3 && randomNumber > valeurInput - 3) {
                 body.style.backgroundImage = bgBrulant;
-                message.textContent = "C'est Brûlant !!!" ;
-            }else if(randomNumber < valeurInput + 6 && randomNumber > valeurInput - 6) {
+                message.textContent = "C'est Brûlant !!! 🔥🔥🔥 ";
+            } else if (randomNumber < valeurInput + 6 && randomNumber > valeurInput - 6) {
                 body.style.backgroundImage = bgChaud;
-                message.textContent = "C'est chaud !!!";
-            }else if(randomNumber < valeurInput + 11 && randomNumber > valeurInput - 11) {
-                body.style.backgroundImage = bgTiède;
-                message.textContent = "C'est tiède !!!";
-            }else {
+                message.textContent = "C'est Chaud ! 🔥 ";
+            } else if (randomNumber < valeurInput + 11 && randomNumber > valeurInput - 11) {
+                body.style.backgroundImage = bgTiede;
+                message.textContent = "C'est Tiède 😐 ";
+            } else {
                 body.style.backgroundImage = bgFroid;
-                message.textContent = "C'est froid !!!";
+                message.textContent = "C'est Froid ❄️ "; 
             }
             vies--;
             verifyLoose();
         }
 
+        actualiseCoeurs(vies);
+
+
     })
+
     const verifyLoose = () => {
-        if(vies === 0) {
+        if (vies === 0) {
             body.style.backgroundImage = bgLoose;
-            body.style.color = 'red';
-            essayerBtn.setAttribute("disabled","");
+            body.style.color = '#990000';
+            essayerBtn.setAttribute("disabled", "");
             message.textContent = `Vous avez perdu. La réponse était ${randomNumber}`;
             rejouerBtn.style.display = "block";
         }
     }
+
+    const actualiseCoeurs = (vies) => {  // total : 6; vies : 4
+        divVies.innerHTML = "";
+        let tableauDeVies = [];
+        for (let i = 0; i < vies; i++) {
+            tableauDeVies.push(coeurPlein);
+        }
+        for (let i = 0; i < totalVies - vies; i++) {
+            tableauDeVies.push(coeurVide);
+        }
+        tableauDeVies.forEach(coeur => {
+            divVies.innerHTML += coeur;
+        })
+    }
+    actualiseCoeurs(vies);
+
+    rejouerBtn.addEventListener('click', () => {
+        message.style.display = 'none';
+        document.location.reload(true);
+    })
+
+
 }
 
+play();
